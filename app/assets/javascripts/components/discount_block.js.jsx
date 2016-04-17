f5e641b553d3e36813b3cfb08a8c9fb7fe51ddd4
Никитин Максим Вадимович <ais-berg@mail.ru>
2016-04-17 23:45:24 +0300

    редактировал ценообразование

--- app/assets/javascripts/components/discount_block.js.jsx
+++ app/assets/javascripts/components/discount_block.js.jsx
@@ -2,15 +2,21 @@ const DiscountBlock = React.createClass({
   render(){
     if (this.props.discount_price){
       
-      var discount_rendered = '';
-      var preliminary_calculation = Math.round(this.props.discount_price / this.props.price * 100);
+      var discount_rendered = ''
+      var preliminary_calculation = Math.round(this.props.discount_price / this.props.price * 100)
       if(preliminary_calculation != 100){
         var procents = 100 - preliminary_calculation;
         discount_rendered = <h5><span className='sale-value'>Скидка {procents}%</span></h5>
       }
 
-      var discount_price_rendered = <span className='sale-price'>{this.props.discount_price} ₽</span>
-      var price = <span className='old-price'>Цена: {this.props.price} ₽</span>
+      var price
+      var discount_price_rendered = ''
+      if(this.props.price > this.props.discount_price){
+        var discount_price_rendered = <span className='sale-price'>{this.props.discount_price} ₽</span>
+        price = <span className='old-price'>Цена: {this.props.price} ₽</span>
+      }else{
+        price = <span className='current-price'>Цена: {this.props.price} ₽</span>
+      }
     }
     else{
       var price = <span className='current-price'>Цена: {this.props.price} ₽</span>
