fbb2840dc67bf3286512984d0e24c4408cd09fb4
Никитин Максим Вадимович <ais-berg@mail.ru>
2016-04-21 15:30:57 +0300

    fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -32,17 +32,5 @@ module ApplicationHelper
       class_name
     end
   end
-
-  def halffull_contacts?(user)
-    phone_contact = 0, vk_contact = 0, facebook_contact = 0, insta_contact = 0
-
-    phone_contact = 1 if user.phone.present?
-    vk_contact = 1 if user.vk_id.present?
-    facebook_contact = 1 if user.facebook_id.present?
-    insta_contact = 1 if user.instagram_id.present?
-
-    sum = phone_contact + vk_contact + facebook_contact + insta_contact
-    sum > 1
-  end
   
 end
