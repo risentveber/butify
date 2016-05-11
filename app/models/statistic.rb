b1aea504eb2d768b890de2ee3db62aef433329f3
risentveber <bsstr@yandex.ru>
2016-05-11 21:11:25 +0300

    fix 1 day in statistic

--- app/models/statistic.rb
+++ app/models/statistic.rb
@@ -2,7 +2,7 @@ class Statistic < ActiveRecord::Base
   self.inheritance_column = false
   enum type: {profile_show: 0, vk_click: 1, instagram_click: 2, facebook_click: 3}
 
-  scope :day, -> { where('statistics.created_at >= ?', 1.days.ago)}
+  scope :day, -> { where('statistics.created_at >= ?', Time.local(Time.now.year, Time.now.month, Time.now.day)}
   scope :week, -> { where('statistics.created_at >= ?', 7.days.ago)}
   scope :month, -> { where('statistics.created_at >= ?', 30.days.ago)}
 end
