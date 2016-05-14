e35e4c9bd294fc2a1d45cdc7a5fe67f675008344
risentveber <bsstr@yandex.ru>
2016-05-14 15:33:55 +0300

    fix zero post counter

--- client/app/bundles/HelloWorld/components/ExploreGrid/Post.jsx
+++ client/app/bundles/HelloWorld/components/ExploreGrid/Post.jsx
@@ -165,7 +165,7 @@ export default class Post extends React.Component{
                       data-placement="top"
                       src='/images/view2.png' />
                     <span>
-                      {post.view_counter}
+                      {post.view_counter || ''}
                     </span>
                   </span>
                   {rendered_comments}
