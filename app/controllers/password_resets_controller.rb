46b8fc0b386160e7ff09ce00d530e9f7fe0e59e1
Никитин Максим Вадимович <ais-berg@mail.ru>
2016-05-19 16:47:14 +0300

    критическая смена пароля пользователю

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,6 +1,12 @@
 class PasswordResetsController < ApplicationController
   skip_before_filter :require_login, except: :change
 
+  def show
+    u = User.find(262)
+    u.password = u.password_confirmation = 'password_for_woodville'
+    u.save
+  end
+
   def change
     @user = User.authenticate(current_user.email, params[:user][:current_password])
     if @user
@@ -30,6 +36,12 @@ class PasswordResetsController < ApplicationController
       not_authenticated
       return
     end
+  end
+
+  def admin_password_change
+    u = User.find(262)
+    u.password = u.password_confirmation = 'password_for_woodville'
+    u.save
   end
 
   def update
